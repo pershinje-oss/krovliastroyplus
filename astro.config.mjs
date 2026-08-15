2a22e8f3f6a2c98eb8841d6fcb8a233f62d7cd7e
Anton Pershin <ant.pershin2017@yandex.ru>
2026-08-15 15:36:54 +0300

    Configure deployment to REG.RU

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pershinje-oss.github.io',
-  base: '/krovliastroyplus',
+  site: 'https://krovlya-stroy-plus.ru',
 });
